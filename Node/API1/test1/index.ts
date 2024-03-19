b6ab014e96b97afda97e93490673aaefa9c50615
MaolinWei <144057115+Maolin-Wei@users.noreply.github.com>
2024-03-19 18:48:41 -0400

    Update index.ts

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -1,4 +1,6 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+const { DefaultAzureCredential } = require('@azure/identity');
+const { SecretClient } = require('@azure/keyvault-secrets');
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
@@ -6,6 +8,19 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    
+    // Initialize Azure Key Vault client
+    const credential = new DefaultAzureCredential();
+    const keyVaultName = process.env.KEY_VAULT_NAME;
+    const kvUrl = `https://${keyVaultName}.vault.azure.net/`;
+    const secretClient = new SecretClient(kvUrl, credential);
+    try {
+        const secretName = "Secret2";
+        const retrievedSecret = await secretClient.getSecret(secretName);
+        context.log(`Secret2 = This is ${retrievedSecret.value}`);
+    } catch (error) {
+        context.log(`Error retrieving Secret2: ${error}`);
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */

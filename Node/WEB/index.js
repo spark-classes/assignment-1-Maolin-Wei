e92809fdade851bc1178e831cdb4089590a928d8
MaolinWei <144057115+Maolin-Wei@users.noreply.github.com>
2024-03-19 16:12:02 -0400

    Update index.js

--- Node/WEB/index.js
+++ Node/WEB/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const { DefaultAzureCredential } = require('@azure/identity');
+const { SecretClient } = require('@azure/keyvault-secrets');
+
 const app = express();
 const port = process.env.PORT || 3000
 
+// Initialize Azure Key Vault client
+const credential = new DefaultAzureCredential();
+const keyVaultName = process.env.KEY_VAULT_NAME;
+const kvUrl = `https://${keyVaultName}.vault.azure.net/`;
+
+const client = new SecretClient(kvUrl, credential);
+
 app.use('/', express.static('frontend/build'));
 
-app.get('/api', (req, res) => {
-  res.send('Hello, world!');
+app.get('/api', async (req, res) => {
+  try {
+    const secretName = 'Secret1';
+    const retrievedSecret = await client.getSecret(secretName);
+    res.send(`Hello, world! Secret value: ${retrievedSecret.value}`);
+  } catch (error) {
+    console.error('Error accessing secret from Key Vault:', error);
+    res.status(500).send('Error accessing secret from Key Vault');
+  }
 });
 
 app.listen(port, () => {

7753869b5c9e6490a28479d971194687ead86c64
MaolinWei <144057115+Maolin-Wei@users.noreply.github.com>
2024-03-20 23:53:15 -0400

    Update index.js

--- Node/API2/QueueProcessor/index.js
+++ Node/API2/QueueProcessor/index.js
@@ -1,3 +1,20 @@
+const { DefaultAzureCredential } = require('@azure/identity');
+const { SecretClient } = require('@azure/keyvault-secrets');
+
 module.exports = async function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
+
+    // Initialize Azure Key Vault client
+    const credential = new DefaultAzureCredential();
+    const keyVaultName = process.env.KEY_VAULT_NAME;
+    const kvUrl = `https://${keyVaultName}.vault.azure.net/`;
+    const secretClient = new SecretClient(kvUrl, credential);
+    
+    try {
+        const secretName = "Secret3";
+        const retrievedSecret = await secretClient.getSecret(secretName);
+        context.log(`Secret3 = This is ${retrievedSecret.value}`);
+    } catch (error) {
+        context.log(`Error retrieving Secret3: ${error}`);
+    }
 };

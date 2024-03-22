9f17471736a041fee458b107300778cd52b834b2
MaolinWei <144057115+Maolin-Wei@users.noreply.github.com>
2024-03-21 22:22:30 -0400

    Update index.ts

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -6,7 +6,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully. This is secret2: " + process.env.secret2
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
     
     // Initialize Azure Key Vault client
